a276360eb5358210c0a6319294094161a1768e58
Tilen Stermecki <tilen.stermecki@gmail.com>
2023-09-07 20:47:13 +0200

    pre-release modifications

--- .history/include/timer/timer_20230907200919.cpp
+++ .history/include/timer/timer_20230907200919.cpp
@@ -1,12 +1,12 @@
 #include "timer/timer.h"
 
 Timer::Timer() {
-    start = std::chrono::steady_clock::now();
+    start = std::chrono::high_resolution_clock::now();
     stop = start;
 }
 
 double Timer::interval() {
-    start = std::chrono::steady_clock::now();
+    start = std::chrono::high_resolution_clock::now();
     double dt = std::chrono::duration_cast<std::chrono::nanoseconds>(start - stop).count() * 0.000000001;
     stop = start;
     return dt;
